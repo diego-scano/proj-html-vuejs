b2ca408dcd197cd546a04e1fb90737e16752a48f
Diego <diego.scano77@gmail.com>
2021-02-18 19:08:09 +0100

    3 sections + header vuejs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,36 @@
 new Vue ({
   el: '#root',
   data: {
-    
+    mainNavbar: [
+      {
+        name: 'HOME',
+        link: '#'
+      },
+      {
+        name: 'ABOUT',
+        link: '#'
+      },
+      {
+        name: 'SERVICES',
+        link: '#'
+      },
+      {
+        name: 'PROCESS',
+        link: '#'
+      },
+      {
+        name: 'TEAM',
+        link: '#'
+      },
+      {
+        name: 'BLOG',
+        link: '#'
+      },
+      {
+        name: 'GET IN TOUCH',
+        link: '#',
+        class: 'button-1'
+      }
+    ]
   }
 })
