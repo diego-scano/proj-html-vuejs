e5052bd21882d61b3e8d6da62ae526ba9851ef33
Diego <diego.scano77@gmail.com>
2021-02-21 14:32:37 +0100

    es completo (no refactoring)

--- js/script.js
+++ js/script.js
@@ -31,6 +31,95 @@ new Vue ({
         link: '#',
         class: 'button-1'
       }
+    ],
+    footerMenu: [
+      {
+        title: 'About',
+        list: [
+          {
+            name: 'The company',
+            link: '#'
+          },
+          {
+            name: 'Institutional',
+            link: '#'
+          },
+          {
+            name: 'Social & Events',
+            link: '#'
+          },
+          {
+            name: 'Innovation',
+            link: '#'
+          },
+          {
+            name: 'Environment',
+            link: '#'
+          },
+          {
+            name: 'Technology',
+            link: '#'
+          }
+        ]
+      },
+      {
+        title: 'Services',
+        list: [
+          {
+            name: 'Audit & Assurance',
+            link: '#'
+          },
+          {
+            name: 'Financial Advisory',
+            link: '#'
+          },
+          {
+            name: 'Analytics M&A',
+            link: '#'
+          },
+          {
+            name: 'Middle Marketing',
+            link: '#'
+          },
+          {
+            name: 'Legal Consulting',
+            link: '#'
+          },
+          {
+            name: 'Regulatory Risk',
+            link: '#'
+          }
+        ]
+      },
+      {
+        title: 'Support',
+        list: [
+          {
+            name: 'Responsibility',
+            link: '#'
+          },
+          {
+            name: 'Terms of Use',
+            link: '#'
+          },
+          {
+            name: 'About Cookies',
+            link: '#'
+          },
+          {
+            name: 'Privacy Policy',
+            link: '#'
+          },
+          {
+            name: 'Accessibility',
+            link: '#'
+          },
+          {
+            name: 'Information',
+            link: '#'
+          }
+        ]
+      }
     ]
   }
 })
